8502257cd3f300feed67c08db20d50ab9894861c
Baptiste <bnonat@mkdirect.fr>
2023-07-27 00:53:42 +0200

    Remove vertical padding on Layout mobile

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -23,7 +23,7 @@ const Layout = ({ children }: Props) => {
           lightTheme ? "bg-white text-gray-900" : "bg-gray-900 text-white"
         )}`}
       >
-        <div className="mx-auto max-w-screen-xl px-4 py-10 flex h-screen items-center">
+        <div className="mx-auto max-w-screen-xl px-4 lg:py-10 flex h-screen items-center">
           <div className="mx-auto max-w-3xl text-center">{children}</div>
         </div>
         <button onClick={handleThemeChange} className=" absolute bottom-1 left-1"
