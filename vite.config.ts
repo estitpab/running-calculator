f69750857f4b1dfb6fa53a5bd45b6b271bf9f059
Baptiste <bnonat@mkdirect.fr>
2023-07-27 17:02:18 +0200

    Add vite-plugin-pwa with pwa images

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      includeAssets: ['running-calculator.svg', 'pwa-192x192.png', 'pwa-512x512'],
+      manifest: {
+        name: 'Running Calculator',
+        short_name: 'MyApp',
+        description: 'My Awesome App description',
+        theme_color: '#111826',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    }),
+  ],
+});
